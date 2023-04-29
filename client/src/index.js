f8c3e3befa19b9343a2b36bf5f7e07c65fb4366c
Rabiul1348025 <rabiulh.11@hotmail.com>
2023-04-29 21:33:41 +0300

    Deployment Update

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { AuthContextProvider } from './context/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // Structure handling
   <React.StrictMode>
     <AuthContextProvider>
       
